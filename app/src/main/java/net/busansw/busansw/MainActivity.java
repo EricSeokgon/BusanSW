5ee73504191294a4de48a47e187a1d84d6258515
hadesyi <hadesleeq@gmail.com>
2015-11-18 17:57:09 +0900

    Initial commit

---
+++ app/src/main/java/net/busansw/busansw/MainActivity.java
@@ -0,0 +1,67 @@
+package net.busansw.busansw;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+public class MainActivity extends Activity  {
+    private WebView mWebView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setLayout();
+
+        // 웹뷰에서 자바스크립트실행가능
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        // SW융합클러스터홈페이지 지정
+        mWebView.loadUrl("http://www.busansw.net");
+        // WebViewClient 지정
+        mWebView.setWebChromeClient(new WebChromeClient());
+        //mWebView.setWebViewClient(new WebViewClientClass());
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    /*
+     * Layout
+     */
+    private void setLayout(){
+        mWebView = (WebView) findViewById(R.id.webview);
+    }
+}
